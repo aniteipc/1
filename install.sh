48b59feb69975a82aff3ea7822f52fc7b89d402f
名叫句号的小伙伴 <45382665+aniteipc@users.noreply.github.com>
2022-04-21 16:03:22 +0800

    1
    1

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash                                                                                                           1 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+hwclock --systohc
+sed -i '501s/.//'  /etc/locale.gen
+#ÉèÖÃzh_CH.UTF8
+#sed -i '178s/.//'  /etc/locale.gen
+#ÉèÖÃen_US.UTF8
+locale-gen
+echo "LANG=zh_CH.UTF-8" >> /etc/locale.conf
+#echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "zk" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+echo "127.0.1.1 zk.localdomain  arch" >> /etc/hosts
+echo "root:123" >> | chpasswd
+
+
+pacman -S --noconfirm  bluez bluez-utils  efibootmgr grub sudo vim wget  zsh iwd networkmanager
+#ttf-dejavu  wqy-zenhei wqy-microhei alacritty xorg xorg-xinit i3  pulseaudio
+
+
+systemctl enable bluetooth.service
+systemctl enable NetworkManager.service
+#systemctl enable
+#systemctl enable
+#systemctl enable
+#systemctl enable
+#systemctl enable
