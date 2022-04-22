23e90c9074c58c85a881c26b7cc92eb34ca7e986
名叫句号的小伙伴 <45382665+aniteipc@users.noreply.github.com>
2022-04-22 12:36:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ systemctl enable NetworkManager.service
 #systemctl enable
 #systemctl enable
 
+chsh -s /bin/zhs
 
 grub-mkconfig -o /boot/grub/grub.cfg
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
