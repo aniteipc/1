0996e69be709fd08cea4403bc524b546c16278db
名叫句号的小伙伴 <45382665+aniteipc@users.noreply.github.com>
2022-04-22 15:47:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 hwclock --systohc
-sed -i '501s/.//'  /etc/locale.gen
+#sed -i '501s/.//'  /etc/locale.gen
 #ÉèÖÃzh_CH.UTF8
-#sed -i '178s/.//'  /etc/locale.gen
+sed -i '178s/.//'  /etc/locale.gen
 #ÉèÖÃen_US.UTF8
 locale-gen
-echo "LANG=zh_CH.UTF-8" >> /etc/locale.conf
-#echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+#echo "LANG=zh_CH.UTF-8" >> /etc/locale.conf
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "zk" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1 localhost" >> /etc/hosts
@@ -17,13 +17,13 @@ useradd zk -g wheel -m
 echo "zk:123" | chpasswd
 
 
-pacman -S --noconfirm  bluez bluez-utils  efibootmgr grub sudo vim wget  zsh iwd networkmanager
+pacman -S --noconfirm  bluez bluez-utils  efibootmgr grub sudo vim wget  zsh iwd networkmanager openssh
 #ttf-dejavu  wqy-zenhei wqy-microhei alacritty xorg xorg-xinit i3  pulseaudio
 
 
 systemctl enable bluetooth.service
 systemctl enable NetworkManager.service
-#systemctl enable
+systemctl enable sshd
 #systemctl enable
 #systemctl enable
 #systemctl enable
