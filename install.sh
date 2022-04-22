2267c3968cc426d06120bc8ef9f66b7ddbaf7552
名叫句号的小伙伴 <45382665+aniteipc@users.noreply.github.com>
2022-04-22 11:52:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,13 +13,13 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1 localhost" >> /etc/hosts
 echo "127.0.1.1 zk.localdomain  arch" >> /etc/hosts
 echo "root:123" | chpasswd
+useradd zk -g wheel -m
+echo "zk:123" | chpasswd
 
 
 pacman -S --noconfirm  bluez bluez-utils  efibootmgr grub sudo vim wget  zsh iwd networkmanager
 #ttf-dejavu  wqy-zenhei wqy-microhei alacritty xorg xorg-xinit i3  pulseaudio
 
-grub-mkconfig -o /boot/grub/grub.cfg
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 
 systemctl enable bluetooth.service
 systemctl enable NetworkManager.service
@@ -28,3 +28,7 @@ systemctl enable NetworkManager.service
 #systemctl enable
 #systemctl enable
 #systemctl enable
+
+
+grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
